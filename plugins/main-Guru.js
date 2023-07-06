a5bb86bf4f73c1335523983d3d2ef191e5a47568
Som <120825482+OpSom3@users.noreply.github.com>
2023-07-06 12:52:52 +0530

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -3,31 +3,18 @@ let handler = async m => m.reply(`
 ≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
 
 ─────────────
-▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-
-▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
-
-▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
+▢ Join owner group
+https://chat.whatsapp.com/DF3fnIHbFxWEY3bqUAf7Is
 
 ─────────────
-≡ Disabled links? enter here! 
+▢ insta id
+ https://instagram.com/_just_an_weeb_?igshid=NGExMmI2YTkyZg==
 
-▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-─────────────
-▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
-
-▢ *YouTube*
-• https://www.youtube.com/@Asliguru
 
 
 `.trim())
-handler.help = ['gpguru']
+handler.help = ['gpnexxer']
 handler.tags = ['main']
-handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+handler.command = ['groups', 'ownerinfo', 'ownergp', 'ngp', 'gpnex'] 
 
 export default handler
